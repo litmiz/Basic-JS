7f316414e165802e5aa83e5f576ab2134fec1ac8
litmiz <litmiz07@gmail.com>
2020-04-29 19:05:43 +0300

    Added more functions
    Added the functions:
    *ex2
    *ex3

--- exercises.js
+++ exercises.js
@@ -1,6 +1,6 @@
 function ex1() {
    var num1 = Number(prompt('Please enter a number'));
-   var num2 = Number(prompt('Please enter a number'));
+   var num2 = Number(prompt('Please enter another number'));
 
    if(num1 === num2) {
        console.log('BOOM')
@@ -10,3 +10,16 @@ function ex1() {
    }   
 }
 
+function ex2() {
+    var num1 = Number(prompt('Please enter a number'));
+    var num2 = Number(prompt('Thanks, please enter another number'));
+
+    console.log(`The sum is: ${parseInt(num1 + num2)}`)
+}
+
+function ex3() {
+    var num1 = Number(prompt('Please enter a number'));
+    var num2 = Number(prompt('You are Great!, please enter another number'));
+
+    console.log(`The product is: ${parseInt(num1 * num2)}`)
+}
