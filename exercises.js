e9f8b0504783199873f61cda59a934fa2daf42b4
litmiz <litmiz07@gmail.com>
2020-04-29 18:53:16 +0300

    Create HTML + Inline style and create JS file
    Added function ex1

---
+++ exercises.js
@@ -0,0 +1,12 @@
+function ex1() {
+   var num1 = Number(prompt('Please enter a number'));
+   var num2 = Number(prompt('Please enter a number'));
+
+   if(num1 === num2) {
+       console.log('BOOM')
+   }
+   else {
+       console.log('Try Again')
+   }   
+}
+
